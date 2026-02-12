677ff5eaf55b9fa30e74525dd1eeccc7b0611731
Ramanakumar Sankar <ramanakumar.shankar@gmail.com>
2026-02-12 10:02:56 -0800

    switching to kalasiris and pvl

--- get_jupiter_cubes.py
+++ get_jupiter_cubes.py
@@ -2,78 +2,72 @@
 This script will copy over relevant image files and convert them from PDS format to Cassini ISIS3 format
 """
 
-import datetime
 import glob
 import os
-import re
 import shutil
 
+import pvl
+import pytz
 import tqdm
 from dateutil import parser
-from pysis.isis import ciss2isis
+from kalasiris import ciss2isis
 
 # get the list of PDS labels that we need to process
 # labels = sorted(glob.glob('../cassini/data/raw_data/*/*.LBL'))
-labels = sorted(glob.glob('../coiss_1002/data/*/*.LBL'))
+labels = sorted(glob.glob('../cassini/coiss_1002/data/*/*.LBL'))
 
 # get the existing LBLs in the processing folder (useful when restarting code)
 jupiter_lbls = sorted(glob.glob('jupiter_imgs/*.LBL'))
 
 # search patterns to find the target name and filter
-target_pattern = r'TARGET_NAME = "(\S+)"'
-filter_pattern = r"FILTER_NAME = \(\"(?:(?:CL\d)|(\w+))\",\"(?:(?:CL\d)|(\w+))\"\)"
-image_time_pattern = r"IMAGE_TIME = \"?([^Z\"\n]+)"
 
 # time mask to process data only between these time stamps
-start = parser.parse("2000-10-30")
-end = parser.parse("2000-11-17")
+start = pytz.utc.localize(parser.parse("2000-10-30T00:00:00"))
+end = pytz.utc.localize(parser.parse("2000-11-17T00:00:00"))
 
-for label in tqdm.tqdm(labels):
-    filename = os.path.basename(label)
 
-    with open(label, 'r') as infile:
-        label_data = infile.read()
+filters_all = []
 
-    matches = re.findall(target_pattern, label_data)
-    filter_matches = list(re.findall(filter_pattern, label_data)[0])
-    filter_matches = list(filter(lambda e: e != '', filter_matches))
-    time_matches = re.findall(image_time_pattern, label_data)
+for label_file in tqdm.tqdm(labels, dynamic_ncols=True):
+    filename = os.path.basename(label_file)
 
-    if len(matches) > 0:
-        if matches[0].lower() == 'jupiter':
-            # make sure we're getting a filter (we want to ignore open filters)
-            if len(filter_matches) > 1 or len(filter_matches) == 0:
-                print(f"No filters found for {label}")
-                print(filter_matches)
-                continue
+    labels = pvl.load(label_file)
 
-            filter_name = filter_matches[0]
+    if (
+        labels['TARGET_NAME'].lower() == 'jupiter'
+        and 'NARROW ANGLE' in labels['INSTRUMENT_NAME']
+    ):
+        filters = labels['FILTER_NAME']
+        filters_all.append(tuple(filters))
+        # make sure we're getting a filter (we want to ignore open filters)
 
-            if len(time_matches) > 1 or len(time_matches) == 0:
-                print(f"No time found for {label}")
-                print(time_matches)
-                continue
+        if filters[0] == "BL1" and filters[1] == "CL2":
+            filter_name = "BL1"
+        elif filters[0] == "CL1" and filters[1] == "GRN":
+            filter_name = "GRN"
+        elif filters[0] == "CL1" and filters[1] == "CB2":
+            filter_name = "CB2"
+        else:
+            # print(filters, labels['INSTRUMENT_NAME'])
+            continue
 
-            image_time = time_matches[0]
-            start_time = datetime.datetime.strptime(
-                image_time.strip()[:-4],
-                "%Y-%jT%H:%M:%S",
+        start_time = labels['IMAGE_TIME']
+
+        # process only between the given times
+        if start_time > start and start_time < end:
+            # create the folder and move images over to the processing folder
+            if not os.path.exists(f'jupiter_imgs/{filter_name}'):
+                os.makedirs(f'jupiter_imgs/{filter_name}')
+
+            shutil.copyfile(label_file, f'jupiter_imgs/{filter_name}/{filename}')
+            shutil.copyfile(
+                label_file.replace('.LBL', '.IMG'),
+                f'jupiter_imgs/{filter_name}/{filename.replace(".LBL", ".IMG")}',
             )
 
-            # process only between the given times
-            if start_time > start and start_time < end:
-                # create the folder and move images over to the processing folder
-                if not os.path.exists(f'jupiter_imgs/{filter_name}'):
-                    os.makedirs(f'jupiter_imgs/{filter_name}')
+            jupiter_lbls.append(f'jupiter_imgs/{filter_name}/{filename}')
 
-                shutil.copyfile(label, f'jupiter_imgs/{filter_name}/{filename}')
-                shutil.copyfile(
-                    label.replace('.LBL', '.IMG'),
-                    f'jupiter_imgs/{filter_name}/{filename.replace(".LBL", ".IMG")}',
-                )
-
-                jupiter_lbls.append(f'jupiter_imgs/{filter_name}/{filename}')
-
+print(set(filters_all))
 
 # now loop over all files in the processing directory and convert them to ISIS3 format
 for label in tqdm.tqdm(jupiter_lbls):
